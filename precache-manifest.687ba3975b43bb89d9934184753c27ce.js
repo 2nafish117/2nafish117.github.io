b0c082489b6dea0d47bd767abf3f5b9a79f8634d
shashank <shashank.shashank.c@gmail.com>
2020-03-15 17:53:31 +0530

    Updates

---
+++ precache-manifest.687ba3975b43bb89d9934184753c27ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342d5ffb8133fd2095b166babeba89f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb92429ad89e5ea9f2fd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f0ae85d089fcb19653a",
+    "url": "/static/js/2.ccf427b6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ccf427b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb92429ad89e5ea9f2fd",
+    "url": "/static/js/main.7b1ac5f1.chunk.js"
+  },
+  {
+    "revision": "f7ac9df804a4b77171f0",
+    "url": "/static/js/runtime-main.e737e07b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
